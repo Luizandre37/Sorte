fc746bb1189847875ded2e8e5dade82279859404
Oem <luizandremaceno150@gmail.com>
2021-06-09 12:31:28 -0300

    Start commit

---
+++ app/src/main/java/com/teclud/sorteio/MainActivity.java
@@ -0,0 +1,28 @@
+package com.teclud.sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumero(View view){
+
+        TextView texto = findViewById(R .id.textoResultado);
+
+        int x = new Random().nextInt(10);
+
+        texto.setText("numero:" + x );
+    }
+
+}
